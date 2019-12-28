4ebab0ac29aecabc384fddec2ed96869463e8646
Archit Garg <archit.g1998@gmail.com>
2019-12-29 01:06:24 +0530

    All Set to be Rich

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Archit Garg on 13/10/19.
+//  Copyright © 2019 Archit tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
